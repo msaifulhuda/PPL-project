6bbddc0fd885495f8dfba569bf39f69068bcf0b0
meinhere <sabilahmad615@gmail.com>
2024-11-01 21:49:50 +0700

    feat(lms): icon-sidebar + konten dikit

--- PPL/resources/views/guru/lms/index.blade.php
+++ PPL/resources/views/guru/lms/index.blade.php
@@ -1,10 +1,19 @@
 <x-app-guru-layout>
-    <x-slot name="header_content">
-        <h1>{{ __('Data LMS') }}</h1>
-
-        {{-- <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="{{ route('dashboard.lms') }}">Dashboard</a></div>
-        </div> --}}
-    </x-slot>
-
+    <div class="p-2 mx-4 my-6 bg-white shadow rounded-xl md:flex md:items-center md:justify-between md:p-6 xl:p-6">
+        {{-- create breadcrumb --}}
+        <nav class="flex" aria-label="Breadcrumb">
+            <ol class="flex px-6 space-x-4">
+                <li class="flex">
+                    <a href="{{ route('guru.dashboard') }}" class="text-gray-400 hover:text-gray-500">
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="flex">
+                    <a href="{{ route('guru.dashboard') }}" class="font-semibold text-gray-700">
+                        <span>LMS</span>
+                    </a>
+                </li>
+            </ol>
+        </nav>
+    </div>
 </x-app-guru-layout>
