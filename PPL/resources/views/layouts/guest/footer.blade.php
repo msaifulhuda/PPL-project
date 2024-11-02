f6d20b9ba24b1114df8f02ee2d28b584fd101ad5
ham-zettt <ilhamzakaria3024@gmail.com>
2024-11-02 19:13:23 +0700

    add(layout beranda)

---
+++ PPL/resources/views/layouts/guest/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-gray-800 py-8">
+    <div class="container mx-auto px-4 text-center text-white">
+        <p>© 2024 Sistem Sekolah Terintegrasi . SMPN 71 Kamal</p>
+    </div>
+</footer>
