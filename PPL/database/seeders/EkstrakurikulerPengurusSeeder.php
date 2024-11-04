bbfaaa800db8e7e2e48dd58553917a5bc1a0f030
Ranwiesiel <87268415+Ranwiesiel@users.noreply.github.com>
2024-11-04 12:13:47 +0700

    edit(seed ekstra): tambah dan edit seeder untuk ekstrakurikuler

--- PPL/database/seeders/EkstrakurikulerPengurusSeeder.php
+++ PPL/database/seeders/EkstrakurikulerPengurusSeeder.php
@@ -19,61 +19,71 @@ class EkstrakurikulerPengurusSeeder extends Seeder
         $guruid = [session('guruid2'), session('guruid7'), session('guruid8'), session('guruid9')];
 
         // Create Ekstrakurikuler entries and store their IDs
-        $ekstrakurikulerIds = [];
+        $id_ekstra1 = Str::uuid();
+        $id_ekstra2 = Str::uuid();
+        $id_ekstra3 = Str::uuid();
+        $id_ekstra4 = Str::uuid();
+        session(['id_ekstra1' => $id_ekstra1, 'id_ekstra2' => $id_ekstra2, 'id_ekstra3' => $id_ekstra3, 'id_ekstra4' => $id_ekstra4]);
 
-        $ekstrakurikulerIds[] = Ekstrakurikuler::create([
-            'id_ekstrakurikuler' => Str::uuid(),
+        Ekstrakurikuler::create([
+            'id_ekstrakurikuler' => $id_ekstra1,
             'guru_id' => $guruid[0],
             'nama_ekstrakurikuler' => 'Pramuka',
             'deskripsi' => 'Pramuka adalah kegiatan ekstrakurikuler yang bertujuan untuk membentuk karakter siswa.',
             'gambar' => 'test',
-        ])->id_ekstrakurikuler;
+        ]);
 
-        $ekstrakurikulerIds[] = Ekstrakurikuler::create([
-            'id_ekstrakurikuler' => Str::uuid(),
+        Ekstrakurikuler::create([
+            'id_ekstrakurikuler' => $id_ekstra2,
             'guru_id' => $guruid[1],
             'nama_ekstrakurikuler' => 'Paskibra',
             'deskripsi' => 'Paskibra adalah kegiatan ekstrakurikuler yang bertujuan untuk membentuk karakter siswa.',
             'gambar' => '',
-        ])->id_ekstrakurikuler;
+        ]);
 
-        $ekstrakurikulerIds[] = Ekstrakurikuler::create([
-            'id_ekstrakurikuler' => Str::uuid(),
+        Ekstrakurikuler::create([
+            'id_ekstrakurikuler' => $id_ekstra3,
             'guru_id' => $guruid[2],
             'nama_ekstrakurikuler' => 'PMR',
             'deskripsi' => 'PMR adalah kegiatan ekstrakurikuler yang bertujuan untuk membentuk karakter siswa.',
             'gambar' => '',
-        ])->id_ekstrakurikuler;
+        ]);
 
-        $ekstrakurikulerIds[] = Ekstrakurikuler::create([
-            'id_ekstrakurikuler' => Str::uuid(),
+        Ekstrakurikuler::create([
+            'id_ekstrakurikuler' => $id_ekstra4,
             'guru_id' => $guruid[3],
             'nama_ekstrakurikuler' => 'Tafidz',
             'deskripsi' => 'Tafidz adalah kegiatan ekstrakurikuler yang bertujuan untuk membentuk karakter siswa.',
             'gambar' => '',
-        ])->id_ekstrakurikuler;
+        ]);
 
         // Pengurus
+        $id_pengurus1 = Str::uuid();
+        $id_pengurus2 = Str::uuid();
+        $id_pengurus3 = Str::uuid();
+        $id_pengurus4 = Str::uuid();
+        session(['id_pengurus1' => $id_pengurus1, 'id_pengurus2' => $id_pengurus2, 'id_pengurus3' => $id_pengurus3, 'id_pengurus4' => $id_pengurus4]);
+
         $siswa = [session('siswa2'), session('siswa3'), session('siswa4'), session('siswa5')];
 
         PengurusEkstra::create([
-            'id_pengurus_ekstra' => Str::uuid(),
-            'id_ekstrakurikuler' => $ekstrakurikulerIds[0],
+            'id_pengurus_ekstra' => $id_pengurus1,
+            'id_ekstrakurikuler' => $id_ekstra1,
             'id_siswa' => $siswa[0],
         ]);
         PengurusEkstra::create([
-            'id_pengurus_ekstra' => Str::uuid(),
-            'id_ekstrakurikuler' => $ekstrakurikulerIds[1],
+            'id_pengurus_ekstra' => $id_pengurus2,
+            'id_ekstrakurikuler' => $id_ekstra2,
             'id_siswa' => $siswa[1],
         ]);
         PengurusEkstra::create([
-            'id_pengurus_ekstra' => Str::uuid(),
-            'id_ekstrakurikuler' => $ekstrakurikulerIds[2],
+            'id_pengurus_ekstra' => $id_pengurus3,
+            'id_ekstrakurikuler' => $id_ekstra3,
             'id_siswa' => $siswa[2],
         ]);
         PengurusEkstra::create([
-            'id_pengurus_ekstra' => Str::uuid(),
-            'id_ekstrakurikuler' => $ekstrakurikulerIds[3],
+            'id_pengurus_ekstra' => $id_pengurus4,
+            'id_ekstrakurikuler' => $id_ekstra4,
             'id_siswa' => $siswa[3],
         ]);
     }
