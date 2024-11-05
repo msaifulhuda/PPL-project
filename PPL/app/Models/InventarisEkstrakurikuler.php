181cf48669aafff4b960b366a3281c9d628da697
Ranwiesiel <87268415+Ranwiesiel@users.noreply.github.com>
2024-11-05 21:59:22 +0700

    edit(models): menyesuaiakan models untuk sistem ekstrakurikuler

---
+++ PPL/app/Models/InventarisEkstrakurikuler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Notifications\Notifiable;
+
+
+class InventarisEkstrakurikuler extends Model
+{
+
+    use HasUuids, Notifiable;
+
+    protected $table = 'inventaris_ekstrakurikuler';
+    
+    protected $primaryKey = 'id_inventaris';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_inventaris',
+        'id_ekstrakurikuler',
+        'nama_barang',
+        'stok',
+    ];
+
+    public function ekstrakurikuler()
+    {
+        return $this->belongsTo(Ekstrakurikuler::class, 'id_ekstrakurikuler', 'id_ekstrakurikuler');
+    }
+
+    /**
+     * Relationship with HistoriInventaris
+    */
+    public function historiInventaris()
+    {
+        return $this->hasMany(HistoriInventaris::class, 'id_inventaris', 'id_inventaris');
+    }
+}
