8a6ff457bd6a27820ec5414242b1c0cbfeac9e2a
CakAkbar <abdul.hijjah.akbar123@gmail.com>
2024-11-07 15:41:23 +0700

    fitur crud mata pelajaran dan fitur crud untuk assign guru

--- PPL/app/Http/Controllers/staffakademik/StaffakademikController.php
+++ PPL/app/Http/Controllers/staffakademik/StaffakademikController.php
@@ -10,53 +10,6 @@ use Illuminate\Support\Facades\DB;
 
 class StaffakademikController extends Controller
 {
-    public function index()
-    {
-        $kelas = Kelas::all();
-        return view('staff_akademik.master_kelas', compact('kelas'));
-    }
-
-    public function store(Request $request)
-    {
-        Kelas::create($request->all());
-        return redirect()->route('staffakademik.kelas.index')->with('success', 'Kelas berhasil ditambahkan.');
-    }
-
-    public function update(Request $request, $id)
-    {
-        $kelas = Kelas::findOrFail($id);
-        $kelas->update($request->only(['nama_kelas']));
-        return redirect()->route('staffakademik.kelas.index')->with('success', 'Kelas berhasil diperbarui.');
-    }
-
-    public function destroy($id)
-    {
-        $kelas = Kelas::findOrFail($id);
-        $kelas->delete();
-        return redirect()->route('staffakademik.kelas.index')->with('success', 'Kelas berhasil dihapus.');
-    }
-
-    public function cari(Request $request)
-    {
-        $query = $request->input('search'); // Input pencarian
-        $kelas = Kelas::when($query, function($q) use ($query) {
-            return $q->where('nama_kelas', 'LIKE', "%{$query}%");
-        })->paginate(10)->appends(['search' => $query]); // Fungsi paginate()
-    
-        $noDataMessage = $kelas->isEmpty() ? 'Data yang dicari tidak ada.' : '';
-        return view('staff_akademik.master_kelas', compact('kelas', 'noDataMessage'));
-    }
-
-    
-    
-    
-
-
-    /**
-     * START JADWAL MANAGEMENT
-     */
-
-    //  read jadwal
     public function jadwalIndex()
     {
         $kelas = DB::table('kelas')
