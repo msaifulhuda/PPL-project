08e3866457fd52d82e5722f16fa071c817de3e76
Ranwiesiel <87268415+Ranwiesiel@users.noreply.github.com>
2024-11-08 21:00:49 +0700

    feat(ekstrakurikuler): menambahkan fitur kelola perlengkapan & histori, untuk pengurus & pembina

---
+++ PPL/app/Http/Controllers/pengurusekstra/HistoriPeminjamanController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\pengurusekstra;
+
+use App\Http\Controllers\Controller;
+use App\Models\HistoriInventaris;
+use App\Models\InventarisEkstrakurikuler;
+use Illuminate\Http\Request;
+
+class HistoriPeminjamanController extends Controller
+{
+    public function index($id){
+        $id_inventaris = $id;
+        $items = HistoriInventaris::where('id_inventaris', $id)->paginate(10);
+        $barang = InventarisEkstrakurikuler::where('id_inventaris', $id)->value('nama_barang');
+        return view('pengurus_ekstra.perlengkapan.histori', compact(['items', 'id_inventaris', 'barang']));
+    }
+
+    public function store(Request $request){
+
+        $request->validate([
+            'id_inventaris' => 'required',
+            'keterangan' => 'required|string|max:255',
+            'jumlah' => 'required|integer',
+            'histori_keluar' => 'required',
+        ]);
+
+        HistoriInventaris::create($request->all());
+
+        return redirect()->route('pengurus_ekstra.histori', $request->id_inventaris)->with('success', 'Item created successfully.');
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'id_inventaris' => 'required',
+            'keterangan' => 'required|string|max:255',
+            'jumlah' => 'required|integer',
+            'histori_keluar' => 'required',
+            'histori_masuk' => 'required',
+        ]);
+
+        $item = HistoriInventaris::findOrFail($id);
+        $item->update($request->all());
+
+        return redirect()->route('pengurus_ekstra.histori', $request->id_inventaris)->with('success', 'Item updated successfully.');
+    }
+
+    public function destroy($id){
+        $item = HistoriInventaris::findOrFail($id);
+        $id_inventaris = $item->id_inventaris;
+        $item->delete();
+
+        return redirect()->route('pengurus_ekstra.histori', $id_inventaris)->with('success', 'Item deleted successfully.');
+    }
+}
