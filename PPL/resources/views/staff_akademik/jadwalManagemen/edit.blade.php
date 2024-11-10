8477b2aa401887db4f6eac16eaa36164c6c56c90
Glendy Hernandez <glendyhernandezpmg@gmail.com>
2024-11-10 15:50:30 +0700

    feat(tampilan jadwal management)

--- PPL/resources/views/staff_akademik/jadwalManagemen/edit.blade.php
+++ PPL/resources/views/staff_akademik/jadwalManagemen/edit.blade.php
@@ -5,15 +5,69 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                
-                <!-- Tombol Back -->
-                <a href="{{ route('staff_akademik.jadwal') }}" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 mb-4 inline-block">
-                    Kembali
-                </a>
+    <div class="grid grid-cols-1 px-4 pt-6 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
+        {{-- HEADER --}}
+        <div class="mb-4 col-span-full xl:mb-2">
+            <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+            <nav class="flex mb-5" aria-label="Breadcrumb">
+                <ol class="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
+                    <li class="inline-flex items-center">
+                        <a href="{{ route('staff_akademik.dashboard') }}"
+                            class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-primary-500">
+                            <svg class="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                                </path>
+                            </svg>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                            <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            <a href="{{ route('staff_akademik.jadwal') }}"
+                                class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-primary-500">Kelola Jadwal</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                            <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            <a href="#"
+                                class="ml-1 text-gray-700 hover:text-primary-600 md:ml-2 dark:text-gray-300 dark:hover:text-primary-500">Edit Jadwal</a>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+            
+            <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Edit Jadwal
+            </h1>
+            <p class="mb-2 text-black-300 dark:text-black-200">Ini merupakan halaman Edit Jadwal</p>
+            <div class="flex items-center space-x-4">
+                <!-- KEMBALI -->
+                <button onclick="window.location.href='{{ route('staff_akademik.jadwal') }}'"
+                class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800" 
+                data-modal-target="authentication-modal" data-modal-toggle="authentication-modal">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Kembali
+                </span>
+                </button>
+            </div>
+        </div>
 
+        {{-- KONTEN --}}
+        <div class="col-span-full xl:col-auto">
+            <div class="p-4 mb-4 space-y-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
                 <!-- Form Edit Jadwal -->
                 <form action="{{ route('staff_akademik.jadwal.update', $jadwal->id_kelas_mata_pelajaran) }}" method="POST">
                     @csrf
@@ -67,4 +121,5 @@
             </div>
         </div>
     </div>
+
 </x-staffakademik-layout>
