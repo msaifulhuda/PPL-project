2edcdd109fd7c874151f8fd7c6bf026ef0401c88
Ilham Z <ilhamzakaria3024@gmail.com>
2024-11-12 17:38:40 +0700

    feat(halaman beranda):ubah navigation

--- PPL/routes/web.php
+++ PPL/routes/web.php
@@ -202,7 +202,7 @@ Route::group(['prefix' => 'staff_perpus', 'middleware' => ['staff_perpus']], fun
     Route::get('/buku/{id}/edit', [StaffperpusController::class, 'editbuku'])->name('staff_perpus.buku.edit');
     Route::put('/buku/{id}', [StaffperpusController::class, 'updatebuku'])->name('staff_perpus.buku.update');
     Route::delete('/buku/{id}', [StaffperpusController::class, 'destroybuku'])->name('staff_perpus.buku.destroy');
-    
+
 // >>>>>>>>> Temporary merge branch 2
 });
 
@@ -272,7 +272,7 @@ Route::group(['prefix' => 'guru', 'middleware' => ['guru']], function () {
 
         // Anggota Ekstrakurikuler
         Route::get('/pembina/ekstrakurikuler/anggota', [PembinaAnggotaController::class, 'index'])->name('pembina.anggota');
-        
+
         // Perlengkapan Ekstrakurikuler
         Route::get('/pembina/ekstrakurikuler/perlengkapan', [PembinaekstraPerlengkapanController::class, 'index'])->name('pembina.perlengkapan');
         Route::get('/pembina/ekstrakurikuler/perlengkapan/histori/{id}', [PembinaekstraHistoriPeminjamanController::class, 'index'])->name('pembina.histori');
@@ -322,12 +322,12 @@ Route::group(['prefix' => 'pembina_ekstra', 'middleware' => ['pembina_ekstra']],
 
 
 Route::group(['prefix' => 'ekstrakrikuler'], function () {
-    
+
     Route::get('/', [EkstrakurikulerController::class, 'dashboardEkstra'])->name('ekstrakurikuler.dashboardEkstra');
     Route::group(['middleware' => ['siswa']], function() {
 
         Route::post('/registrasi-ekstrakurikuler', [EkstrakurikulerController::class, 'submitForm'])->name('ekstrakurikuler.submit');
-    
+
         Route::middleware('auth:web-siswa')->group(function () {
         Route::get('/registrasi-ekstra', [YourController::class, 'showRegistrasi'])->name('ekstrakurikuler.registrasi');
     });
