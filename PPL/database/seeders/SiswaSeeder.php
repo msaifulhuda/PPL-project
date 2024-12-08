a0c42a60ba6dff6cfcd2babb9e0d6585a3f2d8ff
Sabil Ahmad <sabilahmad615@gmail.com>
2024-12-08 22:19:23 +0700

    feat(notifikasi): memperbaiki notifikasi wa gateway pada materi

--- PPL/database/seeders/SiswaSeeder.php
+++ PPL/database/seeders/SiswaSeeder.php
@@ -13,7 +13,7 @@ class SiswaSeeder extends Seeder
     public function run(): void
     {
 
-        $nomorWa = ["+6289531419612",  "+6285936191911", "+6287864365113"];
+        $nomorWa = ["+6289531419612",  "+6285936191911", "+6287864365113", "+6285172427944"];
 
 
         Siswa::create([
@@ -25,7 +25,7 @@ class SiswaSeeder extends Seeder
             "role_siswa" => "pengurus"
         ]);
         Siswa::create([
-            "nisn" => "03129592757722",
+            "nisn" => "03129592757723",
             "nama_siswa" => "Abdul Rahem Faqih 2",
             "nomor_wa_siswa" => $nomorWa[1],
             "username" => "faqih2",
@@ -33,17 +33,20 @@ class SiswaSeeder extends Seeder
             "role_siswa" => "pengurus"
         ]);
         Siswa::create([
-            "nisn" => "03129592757722",
+            "nisn" => "03129592757724",
             "nama_siswa" => "Sabil Ahmad",
             "nomor_wa_siswa" => $nomorWa[2],
             "username" => "sabil",
             "password" => bcrypt('password'),
             "role_siswa" => "pengurus"
         ]);
-
-
-
-
-
+        Siswa::create([
+            "nisn" => "03129592757725",
+            "nama_siswa" => "Ronggo Widjoyo",
+            "nomor_wa_siswa" => $nomorWa[3],
+            "username" => "ronggo",
+            "password" => bcrypt('password'),
+            "role_siswa" => "pengurus"
+        ]);
     }
 }
