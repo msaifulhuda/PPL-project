a1eb9e6eedb2f55f76065e94a71a146fab4d49f3
Graell29 <umuchtar0@gmail.com>
2024-12-12 16:27:50 +0700

    tambah judul beranda perpus

--- PPL/resources/views/guru/perpustakaan/index.blade.php
+++ PPL/resources/views/guru/perpustakaan/index.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-semibold">Beranda Perpustakaan</h1>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white">
                     <!-- Search and Filter -->
