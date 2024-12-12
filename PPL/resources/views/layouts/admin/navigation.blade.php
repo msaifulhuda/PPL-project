423d48607e46eaa7fbff2679b9288be384825ef6
ReEkaF <andreeka852@gmail.com>
2024-12-12 17:00:05 +0700

    admin

--- PPL/resources/views/layouts/admin/navigation.blade.php
+++ PPL/resources/views/layouts/admin/navigation.blade.php
@@ -59,7 +59,7 @@
                         <ul class="py-1" role="none">
                             <li>
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    role="menuitem">Dashboard</a>
+                                    role="menuitem">Setting</a>
                             </li>
                             <li>
                                 <form method="POST" action="{{ route('logout') }}">
