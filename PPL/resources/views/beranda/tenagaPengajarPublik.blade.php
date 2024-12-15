4c43bfee78cf108143feabba97bc7eb8ab185b4f
ham-zettt <ilhamzakaria3024@gmail.com>
2024-12-16 00:16:25 +0700

    fix(beranda):kalo misal gambarnya gk ada, ya tinggal pake dummy

--- PPL/resources/views/beranda/tenagaPengajarPublik.blade.php
+++ PPL/resources/views/beranda/tenagaPengajarPublik.blade.php
@@ -7,7 +7,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
                 @foreach($guru as $gurus)
                 <div class="border w-64 bg-white p-4 rounded-lg shadow-lg">
-                    <img src="{{ $gurus->foto_guru }}" alt="{{ $gurus->nama_guru }}" class="w-64 h-56 object-cover rounded-lg mb-4">
+                    <img src="{{ $gurus->foto_guru ?? asset('images/profile-none.jpeg') }}" alt="{{ $gurus->nama_guru }}" class="w-64 h-56 object-cover rounded-lg mb-4">
                     <h3 class="text-center text-lg font-medium text-gray-800">{{ $gurus->nama_guru }}</h3>
                     <p class="text-center text-gray-600">{{ $gurus->gurumatapelajaran->first()->mataPelajaran->nama_matpel ?? " "}}</p>
                 </div>
